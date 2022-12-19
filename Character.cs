d42a074bdd2275f1e9f069649a8ffcb00df02ae0
nh0zhuybmt <nh0zhuybmt@gmail.com>
2022-12-19 22:52:31 +0900

    Add project files.

---
+++ Character.cs
@@ -0,0 +1,262 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace WindowsFormsApp1
+{
+    internal class Character
+    {
+        public enum Directions
+        {
+            Top = 3,
+            Bottom = 0,
+            Left = 1,
+            Right = 2
+        }
+
+        public enum AnimationFrames
+        {
+            FirstFrame = 0,
+            SecondFrame = 1,
+            ThirdFrame = 2
+        }
+
+        public enum ControlModes
+        {
+            PlayerControl,
+            AIControl
+        }
+
+        // Initializing properties
+        private int id;
+        private ControlModes controlMode;       // To allow controlling by player
+        private string imagePath;
+        private Bitmap sourceBitmap;
+        private int frameIndex;                // To handle which frame to be shown when moving
+        private Directions direction;
+        private Size rectSize;
+        private Point position;
+        private Rectangle movableRect;
+        private int movingStep = 5;      // To handle how far character can move each time key event is called
+        private Timer animationTimer;           // To handle the continuation of the animation
+        private Timer frameTimer;               // To handle the change of the frame
+
+        static int directionCount = Enum.GetNames(typeof(Directions)).Length;
+        static int animationFrameCount = Enum.GetNames(typeof(AnimationFrames)).Length;
+
+        public Character(int id, string imagePath, Size mapSize, ControlModes controlMode = ControlModes.AIControl)
+        {
+            // Initial properties
+            this.id = id;
+            this.imagePath = imagePath;
+            this.controlMode = controlMode;
+            this.sourceBitmap = new Bitmap(this.imagePath);
+            this.frameIndex = (int)AnimationFrames.FirstFrame;
+            this.direction = 0;
+            // Calculate frame size (based on animation frame count and direction count)
+            this.rectSize = new Size(
+                width: sourceBitmap.Width / animationFrameCount,
+                height: sourceBitmap.Height / directionCount
+            );
+            this.movableRect = new Rectangle(
+                x: 0, y: 0,
+                width: mapSize.Width - this.rectSize.Width,
+                height: mapSize.Height - this.rectSize.Height
+            );
+            this.animationTimer = new Timer();
+            this.animationTimer.Interval = 50;
+            this.animationTimer.Tick += new EventHandler(AnimationTimer_Tick);
+
+            this.frameTimer = new Timer();
+            this.frameTimer.Interval = 350;
+            this.frameTimer.Tick += new EventHandler(FrameTimer_Tick);
+        }
+
+        public int ID
+        {
+            get
+            {
+                return this.id;
+            }
+            set
+            {
+                if (value >= 0)
+                {
+                    this.id = value;
+                }
+            }
+        }
+
+        public ControlModes ControlMode
+        {
+            get
+            {
+                return this.controlMode;
+            }
+            set
+            {
+                this.controlMode = value;
+            }
+        }
+
+        public Point Position
+        {
+            get
+            {
+                return this.position;
+            }
+            set
+            {
+                this.position = value;
+            }
+        }
+
+        public Directions Direction
+        {
+            get
+            {
+                return this.direction;
+            }
+            set
+            {
+                this.direction = value;
+            }
+        }
+
+        public Size RectSize
+        {
+            get
+            {
+                return this.rectSize;
+            }
+            set
+            {
+                this.rectSize = value;
+            }
+        }
+
+        public Bitmap BitmapObject
+        {
+            get
+            {
+                Bitmap sourceBitmap = new Bitmap(this.imagePath);
+                Rectangle finalRect = new Rectangle(
+                    x: this.frameIndex * this.rectSize.Width,
+                    y: (int)this.direction * this.rectSize.Height,
+                    width: this.rectSize.Width,
+                    height: this.rectSize.Height);
+                System.Drawing.Imaging.PixelFormat format = sourceBitmap.PixelFormat;
+                Bitmap finalBitmap = sourceBitmap.Clone(finalRect, format);
+                return finalBitmap;
+            }
+        }
+
+        public int MovingStep
+        {
+            get
+            {
+                return this.movingStep;
+            }
+            set
+            {
+                this.movingStep = value;
+            }
+        }
+
+        public void Move()
+        {
+            // Move character based on the current direction
+            if (this.IsMovable())
+            {
+                switch (this.direction)
+                {
+                    case Directions.Top:
+                        this.position.Y -= movingStep;
+                        break;
+                    case Directions.Bottom:
+                        this.position.Y += movingStep;
+                        break;
+                    case Directions.Left:
+                        this.position.X -= movingStep;
+                        break;
+                    case Directions.Right:
+                        this.position.X += movingStep;
+                        break;
+                }
+            }
+
+            // Reset animation continuation timer
+            if (this.animationTimer.Enabled)
+            {
+                this.animationTimer.Stop();
+            }
+            this.animationTimer.Start();
+            // Start frame timer only when it is disabled
+            if (!this.frameTimer.Enabled)
+            {
+                this.frameTimer.Start();
+            }
+        }
+
+        private bool IsMovable()
+        {
+            bool exceedTop = this.position.Y - movingStep < 0;
+            bool exceedBottom = this.position.Y + movingStep > this.movableRect.Height;
+            bool exceedLeft = this.position.X - movingStep < 0;
+            bool exceedRight = this.position.X + movingStep > this.movableRect.Width;
+            var exceed = false;
+            switch (this.direction)
+            {
+                case Directions.Top:
+                    exceed = exceedTop;
+                    break;
+                case Directions.Bottom:
+                    exceed = exceedBottom;
+                    break;
+                case Directions.Left:
+                    exceed = exceedLeft;
+                    break;
+                case Directions.Right:
+                    exceed = exceedRight;
+                    break;
+            }
+            return !exceed;
+        }
+
+
+        public void ToNextAnimation()
+        {
+            if (this.frameIndex + 1 < animationFrameCount)
+            {
+                this.frameIndex += 1;
+            }
+            else
+            {
+                this.frameIndex = 0;
+            }
+        }
+
+        private void AnimationTimer_Tick(object sender, EventArgs e)
+        {
+            this.frameTimer.Stop();
+            this.frameIndex = (int)AnimationFrames.FirstFrame;
+        }
+
+        private void FrameTimer_Tick(object sender, EventArgs e)
+        {
+            if (this.frameIndex + 1 < animationFrameCount)
+            {
+                this.frameIndex += 1;
+            }
+            else
+            {
+                this.frameIndex = 0;
+            }
+            this.frameTimer.Start();
+        }
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        static int characterCount = 16;
+        Character[] characterList = new Character[characterCount];
+        int[] stackIndex = new int[characterCount];
+        Character[] sortedCharacters = new Character[characterCount];
+        Timer timer;
+        string projectPath = System.IO.Path.GetFullPath(@"..\..\");
+        bool firstFrame = true;
+        public Form1()
+        {
+            InitializeComponent();
+            this.timer = new Timer();
+            this.timer.Interval = 500;
+            this.timer.Tick += new EventHandler(Timer_Tick);
+            this.timer.Start();
+
+            this.Size = new Size(400, 400);
+            this.ClientSize = this.Size;
+            this.Text = "Kadai";
+            this.DoubleBuffered = true;
+            this.Paint += new PaintEventHandler(Form1_Paint);
+
+            this.KeyDown += new KeyEventHandler(Form1_KeyDown);
+
+            Random random = new Random();
+            for (int i = 0; i < characterCount; i++)
+            {
+                int imageName = random.Next(0, 17);
+                characterList[i] = new Character(
+                    id: i,
+                    imagePath: $"{projectPath}/images/{i}.png",
+                    mapSize: this.ClientSize
+                );
+                if (i == 10)
+                {
+                    characterList[i].ControlMode = Character.ControlModes.PlayerControl;
+                }
+                else
+                {
+                    characterList[i].ControlMode = Character.ControlModes.AIControl;
+                }
+                characterList[i].Position = new Point(
+                    x: random.Next(0, (this.ClientSize.Width - characterList[i].RectSize.Width) / characterList[i].MovingStep) * characterList[i].MovingStep,
+                    y: random.Next(0, (this.ClientSize.Height - characterList[i].RectSize.Height) / characterList[i].MovingStep) * characterList[i].MovingStep
+                );
+                characterList[i].Direction = (int)Character.Directions.Bottom;
+                stackIndex[i] = i;
+            }
+            
+        }
+
+        public void Timer_Tick(object sender, EventArgs e)
+        {
+        }
+
+        public void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            // Bitmap mapBitmap = new Bitmap($"{this.projectPath}/images/map.png");
+            // e.Graphics.DrawImage(mapBitmap, 0, 0);
+
+            // Sort characters' indices based on positions and ID
+            var sortedByY = characterList.OrderBy(characterList => characterList.Position.Y);
+            var sortedByX = sortedByY.ThenBy(characterList => characterList.Position.X);
+            var sorted = sortedByX.ToArray();
+            for (int i = 0; i < sorted.Length; i++)
+            {
+                sortedCharacters[i] = sorted[i];
+            }
+            Console.WriteLine(stackIndex);
+            for (int i = 0; i < sortedCharacters.Length; i++)
+            {
+                Character currentCharacter = sortedCharacters[i];
+                e.Graphics.DrawImage(
+                    image: currentCharacter.BitmapObject,
+                    point: currentCharacter.Position
+                );
+
+            }
+
+            this.firstFrame = false;
+        }
+
+        public void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            for (int i = 0; i < this.characterList.Length; i++)
+            {
+                Character currentCharacter = this.characterList[i];
+                if (currentCharacter.ControlMode == Character.ControlModes.PlayerControl)
+                {
+                    switch (e.KeyCode)
+                    {
+                        case Keys.Up:
+                            currentCharacter.Direction = Character.Directions.Top;
+                            break;
+                        case Keys.Down:
+                            currentCharacter.Direction = Character.Directions.Bottom;
+                            break;
+                        case Keys.Left:
+                            currentCharacter.Direction = Character.Directions.Left;
+                            break;
+                        case Keys.Right:
+                            currentCharacter.Direction = Character.Directions.Right;
+                            break;
+                    }
+                    currentCharacter.Move();
+                }
+            }
+            Refresh();
+        }
+
+        private void swapStackIndex(int firstIndex, int secondIndex)
+        {
+            var _tempIndex = stackIndex[firstIndex];
+            stackIndex[firstIndex] = stackIndex[secondIndex];
+            stackIndex[secondIndex] = _tempIndex;
+        }
+    }
+}
